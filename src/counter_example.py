c2f6f87284440331c9544e152ec3a11dbac2bb32
vprusso <vincentrusso1@gmail.com>
2022-12-14 08:05:08 -0500

    Updating arXiv identifier.

--- src/counter_example.py
+++ src/counter_example.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Counterexample of antidistinguishability conjecture from arXiv:XX."""
+"""Counterexample of antidistinguishability conjecture from arXiv:2206.08313."""
 import numpy as np
 
 from antidist import AntiDist
